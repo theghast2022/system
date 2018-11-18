79b82860e4b0209178fc1be20c4bc4c469f7f210
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-11-18 20:50:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,19 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Welcome Bro ${client.user.tag}!`);
 });
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Code By : ZorexHost`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Code By : ZorexHost `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`$help | XPRO`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+}); 
+
 const prefix = '$'
 
 client.on('message', message => {
