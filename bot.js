9b41480656d2287217398265556f14b14e7818f9
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2018-12-16 01:09:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1103,21 +1103,5 @@ if (message.content.startsWith(adminprefix + 'setT')) {
  
 });
 
-client.on('message', message => { 
-    var prefix = "$";
- let args = message.content.split(' ').slice(1);
-    if(message.content.startsWith(prefix + 'short')) {
-    if(!message.channel.guild) return;  
-
-        googl.setKey('AIzaSyC2Z2mZ_nZTcSvh3QvIyrmOIFP6Ra6co6w');
-        googl.getKey();
-        googl.shorten(args.join(' ')).then(shorturl => {
-            message.channel.send(''+shorturl)
-        }).catch(e=>{
-            console.log(e.message);
-            message.channel.send('Error!');
-        });
-}
-});
 
 client.login(process.env.BOT_TOKEN);
