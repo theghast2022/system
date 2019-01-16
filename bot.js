3bfe9423358440edba39188a226bc6432d0efe49
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2019-01-16 19:25:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Code By : ZorexHost `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$help | in ${client.guilds.size} Servers `,"http://twitch.tv/S-F")
+client.user.setGame(`$help | $inv `,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 }); 
 
