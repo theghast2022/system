1f6660c7117bc39488128bdcf61af0cf7abd24c4
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2019-01-16 16:43:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1208,33 +1208,14 @@ function play(guild, song) {
  
     serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }
- 
-const adminprefix = ">";
-const devs = ['274923685985386496'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-   
-if (message.content.startsWith(adminprefix + 'setgdame')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'setavatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else    
-if (message.content.startsWith(adminprefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
-}
- 
-});
 
+
+client.on('guildCreate', guild => {
+    var embed = new Discord.RichEmbed()
+    .setColor(#0x5500ff)
+    .setDescription(`**شكراً لك لإضافه البوت الى سيرفرك ازا كنت تحتاج اي مساعدة توجه الى الدعم الفني https://discord.gg/p6WKSDq**`)
+        guild.owner.send(embed)
+  });
 
 
 client.login(process.env.BOT_TOKEN);
