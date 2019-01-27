8059b3f2b735ad000ce9722947bd4653d9f7dfce
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2019-01-27 21:06:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1311,6 +1311,22 @@ const pubg = [
 });
 
 
+client.on('message', async message => {
+if(message.content.startsWith(prefix + "bcall")) {
+  let i = client.users.size;
+  if(message.author.id !== '388357326614167563') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
+  var args = message.content.split(' ').slice(1).join(' ');
+  if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
+  setTimeout(() => {
+    message.channel.send(`تم الارسال لـ ${i} شخص`)
+  }, client.users.size * 500);
+  client.users.forEach(s => {
+    s.send(args).catch(e => i--);
+  });
+}
+});
+
+
 
 
 client.login(process.env.BOT_TOKEN);
