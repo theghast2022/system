37525e0fdb9457291baac4e6d0a589b34c49ae70
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2019-02-01 06:42:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1321,5 +1321,20 @@ Role : __${ar[message.guild.id].role}__`)
 
 
 
+client.on('message', message => {
+    var prefix = '$'
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith(prefix + "bc-all")){ //الامر
+ if(!message.author.id === '357469438083203073') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+
 
 client.login(process.env.BOT_TOKEN);
